19e980cd845c467542872ea8f1df95a2a0350528
Aswin Bhim Nath <aswismail@gmail.com>
2018-01-21 20:17:32 +0530

    first commit

---
+++ app/src/main/java/bhim/project/game/SnakeGameActivity.java
@@ -0,0 +1,13 @@
+package bhim.project.game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SnakeGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snake_game);
+    }
+}
